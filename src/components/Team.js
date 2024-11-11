466e4f977234399fbc9bf19cb2faa3ae41682175
Jocke <joakim-johansson99@hotmail.com>
2024-11-11 12:12:51 +0100

    mails added

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,18 +2,19 @@ import React from 'react';
 
 function Team() {
   const members = [
-    { name: 'Fox Hamrén', info: '' },
-    { name: 'Filip Palmqvist', info: '' },
-    { name: 'Joakim Johansson', info: '' },
-    { name: 'Meilin Zhu', info: '' },
-    { name: 'Vilma Van Der Schoot', info: '' },
+    { name: 'Fox Hamrén', info: 'fox.hamren@gmail.com' },
+    { name: 'Filip Palmqvist', info: 'fille.palmqvist@icloud.com' },
+    { name: 'Joakim Johansson', info: 'joakimjocke199@gmail.com' },
+    { name: 'Meilin Zhu', info: 'meilin77@gmail.com' },
+    { name: 'Vilma Van Der Schoot', info: 'vdschoot@hotmail.se' },
   ];
 
   return (
     <div style={{ width: '70%', display: 'flex', justifyContent: 'center', padding: '12px', margin: '0 auto' }}>
       {members.map((member, index) => (
         <div key={index} style={{ flex: 1, padding: '5px', textAlign: 'center', borderRadius: '8px', margin: '5px' }}>
-          <h4>{member.name}</h4>
+          <h4 style={{ fontSize: '14px', margin: '5px 0' }}>{member.name}</h4>
+          <p style={{'fontSize': 10}}>{member.info}</p>
         </div>
       ))}
     </div>

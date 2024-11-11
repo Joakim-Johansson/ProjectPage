2884fcbe70f84b0e625ebe0607e16080e40c83bb
Jocke <joakim-johansson99@hotmail.com>
2024-11-11 12:20:53 +0100

    project goals added

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -5,6 +5,7 @@ function HomePage() {
     return (
       <div className="HomePage">
             <h3>Project Goals</h3>
+            <p>The goal of this project is to convert a physical board game called Mind MGMT into a digital one. The game should support multiplayer functionality and allow multiple people to play simultaneously from different computers. Since the original game requires five players, we want to add the possibility of playing against an AI, in case less than five persons are playing the game. The rules of the digital game will be based on the original ones, but we might deviate from them a bit in order to create a better game experience. </p>
             <h3>Current state of the project</h3>
             <Divider></Divider>
 
